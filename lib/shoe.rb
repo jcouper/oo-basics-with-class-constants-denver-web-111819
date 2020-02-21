4a263c980c6c34ee21feb88e504f645776dde7e7
jcouper <joshcouper@gmail.com>
2020-02-20 19:38:00 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,20 @@
+require 'pry'
+# Shoe
+# BRANDS
+#   keeps track of all brands (FAILED - 1)
+#   only keeps track of unique brands (FAILED - 2)
+
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS.push(brand)
+    BRANDS.uniq!
   end
 
   def cobble
